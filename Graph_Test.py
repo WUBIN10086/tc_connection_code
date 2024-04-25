bb5d77d2a4e1044f27269da225dee5b73cdb62ca
WangXuan_MacBook <linkovo@Huawei-MateBook-Pro-14.local>
2024-04-25 15:57:15 +0900

    [Temp]临时添加画图逻辑

--- Graph_Test.py
+++ Graph_Test.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 
 # Data for plotting
 categories = ['Max Power', 'Optimized Power']
-values = [79.43, 5.01]
+values = [128.23, 106.43]
 
 # Creating the bar plot with a softer color palette and larger figure
-plt.figure(figsize=(8, 8))  # Increased figure size for better visibility
+plt.figure(figsize=(10, 8))  # Increased figure size for better visibility
 plt.bar(categories, values, color=['#3498db', '#2ecc71'])  # Using softer shades of blue and green
-plt.title('Power Comparison', fontsize=25)
-plt.ylabel('Power (mW)', fontsize=18)
+plt.title('Throughput Comparison', fontsize=25)
+plt.ylabel('Throughput (mBps)', fontsize=18)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.ylim(0, max(values) + 10)  # Adjusting y-axis limit to add some space above the tallest bar
