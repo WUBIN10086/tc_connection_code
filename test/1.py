1106f35185a6eaf29c65adbdde5f66b20cf1398a
WU BIN <wubin990225@gmail.com>
2024-05-07 14:54:24 +0900

    add

--- test/1.py
+++ test/1.py
@@ -17,3 +17,38 @@ Ptd = Prd + 20 * math.log10(4 * math.pi / c) + 20 * math.log10(f) + 20 * math.lo
 RSSd = 10*math.log10(Ptd)
 
 print(Ptd,RSSd)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set temperature range
+temperatures = np.linspace(10, 40, 300)
+membership_no_water = []
+membership_water = []
+
+# Calculate membership functions
+for temp in temperatures:
+    if temp <= 20:
+        membership_no_water.append(1)
+    elif 20 < temp < 30:
+        membership_no_water.append((30 - temp) / 10)  # Linear decrease
+    else:
+        membership_no_water.append(0)
+    
+    if temp >= 30:
+        membership_water.append(1)
+    elif 20 < temp < 30:
+        membership_water.append((temp - 20) / 10)  # Linear increase
+    else:
+        membership_water.append(0)
+
+# Plotting
+plt.figure(figsize=(8, 5))
+plt.plot(temperatures, membership_no_water, label='No Water Needed (Low Temperature)', color='blue')
+plt.plot(temperatures, membership_water, label='Water Needed (High Temperature)', color='red')
+plt.title('Fuzzy Logic Membership Functions for Watering Decision')
+plt.xlabel('Temperature (°C)')
+plt.ylabel('Membership Degree')
+plt.legend()
+plt.grid(True)
+plt.show()
